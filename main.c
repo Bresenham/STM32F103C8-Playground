eb08c2628adc8cbda358edc9289f8b57126976bd
Bresenham <gaul@hm.edu>
2018-04-22 09:40:31 +0200

    Initial Commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "stm32f10x.h"
+
+void TIM2_IRQHandler (void) {
+    TIM2->SR &=~TIM_SR_UIF;
+    GPIOC->ODR ^= GPIO_ODR_ODR13;
+}
+
+void timer2_setup() {
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+  
+  TIM2->PSC = 0;
+  TIM2->ARR = 900;
+  TIM2->DIER |= TIM_DIER_UIE;
+  
+  NVIC_EnableIRQ(TIM2_IRQn);
+
+  TIM2->CR1 = TIM_CR1_CEN;
+}
+
+int main()
+{
+  /* Set System-Clock as high as possible */
+  RCC->CFGR = RCC_CFGR_SW_1 | RCC_CFGR_PLLMULL_3 | RCC_CFGR_PLLMULL_2 | RCC_CFGR_PLLMULL_0;
+  RCC->CR = RCC_CR_HSION | RCC_CR_PLLON;
+
+  /* Set GPIO C13 as High-Speed PushPull output */
+  RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
+  GPIOC->CRH |= GPIO_CRH_MODE13_1 | GPIO_CRH_MODE13_0;
+
+  timer2_setup();
+  GPIOC->ODR &=~GPIO_ODR_ODR13;
+  while(1){
+    asm volatile ("nop");
+  }
+}
